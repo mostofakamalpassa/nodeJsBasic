db95e614ab9f4c671839c1ef3748fc6c04f92659
mostofa kamal <mostofakamal1990@gmail.com>
2022-07-26 01:11:28 +0600

    local modules Add

---
+++ index.js
@@ -0,0 +1,7 @@
+// we can use but it is not recommended
+const s1 = require('./moduels/students');
+// but it is recommended
+const {getName, getAge} = require('./moduels/students') 
+
+// console.log(s1.getName());
+// console.log(s1.getAge());
---
+++ moduels/students.js
@@ -0,0 +1,10 @@
+const getName = ()=>{
+    return  "mostofa kamal passa";
+}
+
+const getAge = () => {
+    return "26";
+}
+
+exports.getName = getName;
+exports.getAge = getAge;
