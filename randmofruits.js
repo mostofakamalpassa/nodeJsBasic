a62ceb9a08ca8d7f9643d6cd9860bb3f5eae44ef
mostofa kamal <mostofakamal1990@gmail.com>
2022-07-26 03:00:42 +0600

    node packge initalize add

---
+++ randmofruits.js
@@ -0,0 +1,5 @@
+const getRandomFruits = require('random-fruits-name');
+const randomMovieNames = require('random-movie-names');
+// console.log(getRandomFruits())
+
+console.log(randomMovieNames());
